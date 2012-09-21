9a27420f31f8d8e74229fd78f26d988072607526
Niall O'Higgins <niallo@unworkable.org>
2012-09-21 11:39:22 -0700

    getting pretty saucey

--- worker.js
+++ worker.js
@@ -6,50 +6,96 @@ var RETRIES = 10
 
 function prepare(ctx, cb) {
   console.log("SAUCE prepare")
-  // Start the app, suggesting a port via PORT environment variable
-  var tsh = ctx.shellWrap("npm start")
-  var serverProc = ctx.forkProc({
-    env:{PORT:8031},
-    cmd:tsh.cmd,
-    args:tsh.args,
-  }, function(exitCode) {
-    // Could perhaps be backgrounding itself. This should be avoided.
-    console.log("serverProc exited with code: %d", exitCode)
-  })
+  var tsh = ctx.shellWrap("npm install")
+  ctx.forkProc(ctx.workingDir, tsh.cmd, tsh.args, function(exitCode) {
+    if (exitCode === 0) {
+      ctx.striderMessage("npm install success")
 
-  var tries = 0
-  ctx.striderMessage("Waiting for webserver to come up on localhost:8031...")
-  var intervalId = setInterval(function() {
-    // Check for http status 200 on http://localhost:PORT/
-    request("http://localhost:8031/", function(err, response) {
-      if (!err && response.statusCode == 200) {
-        ctx.striderMessage("Got HTTP 200 on localhost:8031 indicating server is up")
-        clearInterval(intervalId)
-        serverUp()
-      } else {
-        tries++
-        console.log("Error on localhost:8031: %s", err)
-        if (tries >= RETRIES) {
-          ctx.striderMessage("HTTP 200 check on localhost:8031 failed after " + tries + " retries, aborting test run since server not up")
-          clearInterval(intervalId)
-          return cb(1)
-        }
+    }
+    cb(exitCode)
+  })
+}
+
+//
+// `npm install` has succeeded at this point.
+// We run `npm test` and assuming that has passed,
+// then we start the Sauce test process.
+// If `npm test` fails, we don't bother with the overhead of running the Sauce tests.
+function test(ctx, cb) {
+  console.log("SAUCE test")
+  var startPhaseDone = false
+  var tsh = ctx.shellWrap("npm test")
+  // Run 
+  ctx.forkProc(ctx.workingDir, tsh.cmd, tsh.args, function(exitCode) {
+    if (exitCode !== 0) {
+      return cb(exitCode)
+    } else {
+      ctx.striderMessage("npm test success - trying Sauce tests...")
+
+      npmTestPassed()
+    }
+  })
+  function npmTestPassed() {
+    // `npm test` succeeded, so we go through the Sauce tests.
+    //
+    // Start the app, suggesting a port via PORT environment variable
+    var tsh = ctx.shellWrap("npm start")
+    var serverProc = ctx.forkProc({
+      args:tsh.args,
+      cmd:tsh.cmd,
+      cwd:ctx.workingDir,
+      env:{PORT:8031},
+    }, function(exitCode) {
+      // Could perhaps be backgrounding itself. This should be avoided.
+      console.log("serverProc exited with code: %d", exitCode)
+      if (exitCode !== 0 && !startPhaseDone) {
+        // If we haven't already called back with completion,
+        // and `npm start` exits with non-zero exit code,
+        // call back with error and mark done.
+
+        ctx.striderMessage("npm start failed - failing test")
+        startPhaseDone = true
+        return cb(exitCode)
       }
     })
 
+    var tries = 0
+    ctx.striderMessage("Waiting for webserver to come up on localhost:8031...")
+    var intervalId = setInterval(function() {
+      // Check for http status 200 on http://localhost:PORT/
+      request("http://localhost:8031/", function(err, response) {
+        if (startPhaseDone) {
+          clearInterval(intervalId)
+          return
+        }
+        if (!err && response.statusCode == 200) {
+          ctx.striderMessage("Got HTTP 200 on localhost:8031 indicating server is up")
+          startPhaseDone = true
+          clearInterval(intervalId)
+          serverUp()
+        } else {
+          tries++
+          console.log("Error on localhost:8031: %s", err)
+          if (tries >= RETRIES) {
+            ctx.striderMessage("HTTP 200 check on localhost:8031 failed after " + tries + " retries, server not up - failing test")
+            clearInterval(intervalId)
+            startPhaseDone = true
+            return cb(1)
+          }
+        }
+      })
 
-  }, 1000)
 
-  function serverUp() {
-    var tsh = ctx.shellWrap("npm install")
-    ctx.forkProc(ctx.workingDir, tsh.cmd, tsh.args, cb)
+    }, 1000)
+
+    function serverUp() {
+      // Server is up, start Sauce Connector and run the tests via `npm sauce` invocations
+      ctx.striderMessage("Shutting down server")
+      serverProc.kill()
+
+      return cb(1)
+    }
   }
-}
-
-function test(ctx, cb) {
-  console.log("SAUCE test")
-  var tsh = ctx.shellWrap("npm test")
-  ctx.forkProc(ctx.workingDir, tsh.cmd, tsh.args, cb)
 }
 
 
