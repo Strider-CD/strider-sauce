0d323bf3cfa74a58b53230c3e3ae013a1e9a4bb1
Jared Forsyth <jared@jaredforsyth.com>
2013-10-12 17:25:55 -0600

    sauce config working. actual backend is not ready at all though

--- config/config.js
+++ config/config.js
@@ -1,86 +1,62 @@
 
-(function(){
-
-  function organize(browsers) {
-    var oss = {};
-    for (var i=0; i<browsers.length; i++) {
-      if (!oss[browsers[i].os]) {
-        oss[browsers[i].os] = {};
-      }
-      if (!oss[browsers[i].os][browsers[i].long_name]) {
-        oss[browsers[i].os][browsers[i].long_name] = [];
-      }
-      oss[browsers[i].os][browsers[i].long_name].push(browsers[i]);
-      browsers[i].complete_name = completeName(browsers[i]);
+function organize(browsers) {
+  var oss = {};
+  for (var i=0; i<browsers.length; i++) {
+    if (!oss[browsers[i].os]) {
+      oss[browsers[i].os] = {};
     }
-    return oss;
+    if (!oss[browsers[i].os][browsers[i].long_name]) {
+      oss[browsers[i].os][browsers[i].long_name] = [];
+    }
+    oss[browsers[i].os][browsers[i].long_name].push(browsers[i]);
+    browsers[i].complete_name = completeName(browsers[i]);
   }
+  return oss;
+}
 
-  function completeName(version) {
-    return version.os + '-' + version.api_name + '-' + version.short_version;
-  }
+function completeName(version) {
+  return version.os + '-' + version.api_name + '-' + version.short_version;
+}
 
-  function parseName(name) {
-    var parts = name.split('-');
-    return {
-      platform: parts[0],
-      browserName: parts[1],
-      version: parts[2] || ''
-    };
-  }
+function parseName(name) {
+  var parts = name.split('-');
+  return {
+    platform: parts[0],
+    browserName: parts[1],
+    version: parts[2] || ''
+  };
+}
 
-  function serializeName(browser) {
-    return browser.platform + '-' + browser.browserName + '-' + browser.version;
-  }
+function serializeName(browser) {
+  return browser.platform + '-' + browser.browserName + '-' + browser.version;
+}
 
-  function save(url, data, done) {
-    data = _.extend({url: url}, data);
-    data.sauce_browsers = JSON.stringify(data.sauce_browsers);
-    $.ajax({
-      url: '/api/sauce',
-      type: 'POST',
-      data: data,
-      dataType: 'json',
-      success: function (data, ts, xhr) {
-        done(null);
-      },
-      error: function (xhr, ts, e) {
-        if (xhr && xhr.responseText) {
-          var data = $.parseJSON(xhr.responseText);
-          e = data.errors[0];
-        }
-        done(e);
-      }
-    });
-  }
-
-  app.controller('SauceCtrl', ['$scope', function ($scope) {
-    $scope.data = $scope.panelData['strider-sauce'];
-    $scope.completeName = completeName;
-    $scope.operatingsystems = organize(browsers);
+app.controller('SauceCtrl', ['$scope', function ($scope) {
+  $scope.$watch('configs[branch.name].sauce.config', function (value) {
+    $scope.config = value;
+    if (!value) return;
     $scope.browser_map = {};
-    for (var i=0; i<$scope.data.sauce_browsers.length; i++) {
-      $scope.browser_map[serializeName($scope.data.sauce_browsers[i])] = true;
+    if (!value.browsers) {
+      value.browsers = [];
     }
-    $scope.save = function () {
-      $scope.data.sauce_browsers = [];
-      for (var name in $scope.browser_map) {
-        if ($scope.browser_map[name]) {
-          $scope.data.sauce_browsers.push(parseName(name));
-        }
+    for (var i=0; i<value.browsers.length; i++) {
+      $scope.browser_map[serializeName(value.browsers[i])] = true;
+    }
+  });
+  $scope.completeName = completeName;
+  $scope.operatingsystems = organize(browsers || []);
+  $scope.save = function () {
+    $scope.config.browsers = [];
+    for (var name in $scope.browser_map) {
+      if ($scope.browser_map[name]) {
+        $scope.config.browsers.push(parseName(name));
       }
-      save($scope.repo.url, $scope.data, function (err) {
-        if (err) {
-          $scope.error('Failed to save sauce settings: ' + err);
-        } else {
-          $scope.success('Saved sauce settings');
-        }
-        $scope.$root.$digest();
-      });
-    };
-    $scope.clear = function () {
-      $scope.browser_map = {};
-    };
-  }]);
-
-})();
+    }
+    $scope.pluginConfig('sauce', $scope.config, function () {
+    });
+  };
+  $scope.clear = function () {
+    $scope.browser_map = {};
+    $scope.$digest();
+  };
+}]);
