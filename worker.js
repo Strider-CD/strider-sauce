356205c4ae90ceb62d379c6e05844d6bc43a9779
Niall O'Higgins <niallo@frozenridge.co>
2013-10-22 16:24:37 -0700

    make this work. TODO: inter-plugin coordination for strider-jelly, strider-qunit

--- worker.js
+++ worker.js
@@ -69,9 +69,8 @@ function cleanup(ctx, cb) {
 }
 
 
-function test(config, ctx, cb) {
+function prepare(config, ctx, cb) {
 
-  console.log("test")
   var sauceAccessKey = config.access_key
   var sauceUsername = config.username
   var sauceBrowsers = config.browsers
@@ -88,141 +87,65 @@ function test(config, ctx, cb) {
   }
 
   function log(msg) {
-    console.log("log")
     ctx.out(msg)
     console.log(msg)
   }
 
-  HTTP_PORT = ctx.browsertestPort || 8031
-
-
-  log("Waiting for webserver to come up on localhost:" + HTTP_PORT)
-
-  check({url:"http://localhost:"+HTTP_PORT+"/", log:log}, function(err) {
-    if (err) {
-      return cb(1)
-    }
-    serverUp()
-  })
 
   // Start the Sauce Connector. Returns childProcess object.
   function startConnector(username, apiKey, exitCb) {
     var jarPath = path.join(__dirname, "thirdparty", "Sauce-Connect.jar")
-    var jsh = ctx.shellWrap("exec java -Xmx64m -jar " + jarPath + " " + username + " " + apiKey)
+    var jcmd = "java"
+    var jargs = ["-Xmx64m", "-jar", jarPath, username, apiKey]
+    var screencmd = "java -Xmx64m -jar " + jarPath + " [USERNAME] [API KEY]"
     
     ctx.out("Starting Sauce Connector")
     var opts = {
       cwd: ctx.workingDir,
-      cmd: jsh.cmd,
-      args: jsh.args,
-      screencmd: true,
-      env: {}
+      cmd: {
+        command: jcmd,
+        args: jargs,
+        screen: screencmd
+      }
     }
-    connectorProc = ctx.forkProc(opts, exitCb)
+    connectorProc = ctx.cmd(opts, exitCb)
     // Wait until connector outputs "You may start your tests"
-    // before executing Sauce tests
+    // before returning
     connectorProc.stdout.on('data', function(data) {
       console.log(">>", data);
       if (/Connected! You may start your tests./.exec(data) !== null) {
           console.log(">> STRIDER-SAUCE :: TUNNEL READY")
-          var resultsReceived = 0
-          var buildStatus = 0
-          var resultMessages = []
-          var finished = false
-          var tasks = []
-          var sauceTimeoutSeconds = SAUCE_TEST_TIMEOUT / 1000
-          sauceBrowsers.forEach(function(browser) {
-            var worker = wd.remote("ondemand.saucelabs.com", 80, 
-              sauceUsername, sauceAccessKey)
-            var browserId = browser.browserName.replace(' ', '_') + "-" + browser.version + "-" + browser.platform.replace(" ", "-")
-            // ctx.browsertestPort and ctx.browsertestPath come from the `prepare` phase test
-            // plugin - e.g. strider-qunit.
-            var testUrl = "http://localhost:" +
-                ctx.browsertestPort + "/" + browserId + ctx.browsertestPath
-            worker.done = false
-            worker.init({
-              browserName: browser.browserName,
-              version: browser.version,
-              platform: browser.platform,
-              name: ctx.jobData.repo_config.display_url,
-              'idle-timeout': sauceTimeoutSeconds,
-              'max-duration': sauceTimeoutSeconds
-            }, function(err) {
-              if (err) {
-                log("Error creating Sauce worker: ")
-                console.dir(err)
-                return cb(1)
-              }
-              log("Created Sauce worker: " + browserId)
-              worker.title(function(err, title){ // Potentially a selenium bug - wait on the title before doing the redirect
-                log(browserId + " ... " + title + err)
-                worker.get(testUrl, function() {
-                  log("worker -> " + testUrl)
-                })
-              })
-
-            })
-
-            setTimeout(function() {
-              if (!worker.done) {
-                log("ERROR: Timeout of " + SAUCE_TEST_TIMEOUT + " ms exceeded for " + browserId + " - terminating ")
-                ctx.events.emit('testDone', { id: browserId, total:0, failed:1, passed:0, runtime: SAUCE_TEST_TIMEOUT })
-              }
-            }, SAUCE_TEST_TIMEOUT)
-            ctx.events.on('testDone', function(result) {
-              if (finished) return
-              if (result.id === browserId && worker && !worker.done) {
-                tasks.push({id:"sauce", data:{total:result.total, failed: result.failed, passed:result.passed, runtime:result.runtime, id:result.id}})
-                resultMessages.push("Results for tests on " + result.id + ": " + result.total + " total " +
-                  result.failed + " failed " + result.passed + " passed " + result.runtime + " ms runtime") 
-                if (result.failed !== 0) {
-                  buildStatus = 1
-                }
-                log("Terminating Sauce worker: " + browserId)
-                worker.quit()
-                worker.done = true
-                resultsReceived++
-              }
-              // If all the results are in, finish the build
-              if (resultsReceived == sauceBrowsers.length) {
-                finished = true
-                resultMessages.forEach(function(msg) {
-                  log(msg)
-                })
-                cb(buildStatus, tasks)
-              }
-            })
-          })
+          return cb(null, true)
       }
     })
   }
-  // Server is up, start Sauce Connector
-  function serverUp() {
-    console.log("Starting sauce connector")
-    startConnector(sauceUsername, sauceAccessKey,
-      function(exitCode) {
-      console.log("Sauce Connector exited with code: %d", exitCode)
-      // If the connector exited before the cleanup phase has run, it failed to start
-      if (!cleanupRun) {
-        log("Error starting Sauce Connector - failing test")
-        cleanupRun = true
-        fs.unlink(PIDFILE, function() {})
-        return cb(1)
-      }
-    })
-  }
+  console.log("Starting sauce connector")
+  startConnector(sauceUsername, sauceAccessKey,
+    function(exitCode) {
+    console.log("Sauce Connector exited with code: %d", exitCode)
+    // If the connector exited before the cleanup phase has run, it failed to start
+    if (!cleanupRun) {
+      log("Error starting Sauce Connector - failing test")
+      cleanupRun = true
+      fs.unlink(PIDFILE, function() {})
+      return cb(1)
+    }
+  })
 }
 
 
 module.exports = {
   init: function (config, job, context, done) {
-    console.log("config", config)
+    var browsersVar = config.browsers.map(function(b) {
+      return b.platform.replace(' ', '_') + "-" + b.browserName + "-" + b.version
+    }).join(" ")
     done(null, {
-      test: function(ctx, done) {
+      env: {'BROWSERS': browsersVar},
+      prepare: function(ctx, done) {
         if (sauceConfigured(config)) {
           console.log("sauce configured")
         }
-        test(config, ctx, done)
+        prepare(config, ctx, done)
       },
       cleanup: cleanup
     })
