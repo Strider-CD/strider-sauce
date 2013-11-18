d707311aaa6e557a58597547f38da7b5bb3c2cc9
Niall O'Higgins <niallo@frozenridge.co>
2013-11-18 15:05:58 -0800

    no longer bundle Sauce-Connect.jar instead we use an npm preinstall hook to download & extract it.

---
+++ install.js
@@ -0,0 +1,17 @@
+var fs = require('fs')
+var path = require('path')
+var request = require('request')
+var unzip = require('unzip')
+
+var SAUCE_CONNECT_URL = 'http://saucelabs.com/downloads/Sauce-Connect-latest.zip'
+
+console.log("Downloading Sauce-Connect.jar from SauceLabs...")
+request(SAUCE_CONNECT_URL)
+  .pipe(unzip.Parse())
+  .on('entry', function(entry) {
+    if (entry.path === 'Sauce-Connect.jar') {
+      entry.pipe(fs.createWriteStream(path.join(__dirname, 'thirdparty', entry.path)))
+    } else {
+      entry.autodrain()
+    }
+  })
